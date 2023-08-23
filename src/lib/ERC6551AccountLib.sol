ed668d562a3dbcb9fdac0c1ecd9504bc46301dcb
Melissa WU <melissawu@Melissas-MacBook-Air.local>
2023-08-22 20:43:22 -0400

    forge install: openzeppelin-contracts
    v4.9.3

--- src/lib/ERC6551AccountLib.sol
+++ src/lib/ERC6551AccountLib.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/utils/Create2.sol";
+import "openzeppelin-contracts/contracts/utils/Create2.sol";
 import "./ERC6551BytecodeLib.sol";
 
 ////////////////////////////////////////////
